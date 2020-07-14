300d7466d1e239146cafccdcf56bd68273515807
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-07-14 18:06:16 +1000

    Create example2.py

---
+++ Example Project/example2.py
@@ -0,0 +1,13 @@
+import sys #Imports the module named "sys" into the current namespace
+import os #Imports the module named "os" into the current namespace
+
+os.system('cls' if os.name == 'nt' else 'clear') #Clears the screen
+print("Hello!") #Prints Text to the terminal 
+name = input("What is your name? ") #Creates a variable "name" and allows input of text
+print("Nice to meet you " + name) #Prints text and displays input from "name" variable
+
+age = input("How old are you? ")
+print("Age " + age + " nice!")
+
+friend = input("What is your friends name? ")
+print("So your friends with " + friend)
