57d8c200ee95adfe78d2e2bd42a8a7e1c86f0c3f
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-07-14 18:34:39 +1000

    Update example.py

--- Example Project/example.py
+++ Example Project/example.py
@@ -4,4 +4,4 @@ import os #Imports the module named "os" into the current namespace
 os.system('cls' if os.name == 'nt' else 'clear') #Clears the screen
 print("Hello!") #Prints Text to the terminal 
 name = input("What is your name? ") #Creates a variable "name" and allows input of text
-print("Nice to meet you " + name) #Prints text and displays input from "name" variable
+print("Nice to meet you", name) #Prints text and displays input from "name" variable
