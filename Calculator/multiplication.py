c2b6c102fa2e0949e1dff4a5f8e5cda6076b913b
PapaSnags <58458169+PapaSnags@users.noreply.github.com>
2020-08-30 00:50:58 +1000

    Update and rename mutilation.py to multiplication.py

--- Calculator/multiplication.py
+++ Calculator/multiplication.py
@@ -1,4 +1,4 @@
-print("Welcome to the Python Mutilation Calculator")
+print("Welcome to the Python Multiplication Calculator")
 num1 = int(input("Give me a number to multiply "))
 num2 = int(input("Give me another number to multiply "))
 print(num1,"x",num2,"=", num1 * num2)
